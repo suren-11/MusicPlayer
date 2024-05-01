48553ba3a573aee9fa9c3a5c6999b785c2a94c4a
suren-11 <presanth11@gmail.com>
2024-05-01 11:46:09 +0530

    implement storage permission codes

--- app/src/main/java/com/suren/musicPlayer/MainActivity.kt
+++ app/src/main/java/com/suren/musicPlayer/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.suren.musicPlayer
 
 import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
 import com.suren.musicPlayer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -11,6 +13,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        requestRuntimePermission()
         setTheme(R.style.Theme_MusicPlayer)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
@@ -28,6 +31,25 @@ class MainActivity : AppCompatActivity() {
         binding.playlistBtn.setOnClickListener {
             val intent = Intent(this@MainActivity,PlaylistActivity::class.java)
             startActivity(intent)
+        }
+    }
+
+    //for requesting permission
+    private fun requestRuntimePermission(){
+        if(ActivityCompat.checkSelfPermission(this,android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+        !=PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.WRITE_EXTERNAL_STORAGE),13)
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if(requestCode==13){
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                Toast.makeText(this,"Permission Granted",Toast.LENGTH_SHORT).show()
+            }else {
+                ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.WRITE_EXTERNAL_STORAGE),13)
+            }
         }
     }
 }
